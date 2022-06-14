626c78700d6bff2df92f8bd4fb0ef4dac152b588
Mayur Sonkusare <mayursonkusare1155@gmail.com>
2022-06-14 13:59:26 +0530

    second-initial

--- src/Component/Component.js
+++ src/Component/Component.js
@@ -2,6 +2,7 @@ import './Component.css';
 function Component(){
     return(
         <div className='cmp'>
+           <img src="record.jpg" alt="Something is error" ></img>
             <p>hellow students</p>
         </div>
     );
