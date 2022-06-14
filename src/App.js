b2a689c703baf7714c4aba76dd0d6b09da0a831d
Mayur Sonkusare <mayursonkusare1155@gmail.com>
2022-06-14 13:11:34 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import  './App.css';
+import Component from './Component/Component';
+function App(){
+  return(
+    <div>
+      <h1 className='container'>RTC-Component</h1>
+      <Component/><Component/><Component/><Component/>
+    </div>
+    
+  );
+}
+export default App;
---
+++ src/Component/Component.js
@@ -0,0 +1,9 @@
+import './Component.css';
+function Component(){
+    return(
+        <div className='cmp'>
+            <p>hellow students</p>
+        </div>
+    );
+}
+export default Component;
